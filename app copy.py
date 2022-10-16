91ddafb86303b5671e65751f84e6bcfa7d930fd5
Damon <damonjphillips@gmail.com>
2022-10-16 15:20:03 -0500

    save

--- app copy.py
+++ app copy.py
@@ -3,17 +3,17 @@
   {
    "cell_type": "code",
    "execution_count": null,
-   "id": "78c2a2e8",
+   "id": "d1a4040a",
    "metadata": {},
    "outputs": [],
    "source": [
-    "# 10.5.1\n"
+    "# 10.5.1"
    ]
   },
   {
    "cell_type": "code",
    "execution_count": null,
-   "id": "14b2dd9e",
+   "id": "e6e11bfe",
    "metadata": {},
    "outputs": [],
    "source": [
@@ -29,7 +29,7 @@
   {
    "cell_type": "code",
    "execution_count": null,
-   "id": "e3898710",
+   "id": "fada8343",
    "metadata": {},
    "outputs": [],
    "source": [
@@ -40,7 +40,7 @@
   {
    "cell_type": "code",
    "execution_count": null,
-   "id": "48211cb2",
+   "id": "b0ff2be4",
    "metadata": {},
    "outputs": [],
    "source": [
@@ -48,13 +48,13 @@
     "# app.config tells Python that our app will connect to Mongo using a URI. \n",
     "# \"mongodb:...\" is the URI to cnnect app to Mongo. and the db named mars_app.\n",
     "app.config[\"MONGO_URI\"] = \"mongodb://localhost:27017/mars_app\"\n",
-    "mongo = PyMongo(app)\n"
+    "mongo = PyMongo(app)"
    ]
   },
   {
    "cell_type": "code",
    "execution_count": null,
-   "id": "9d1447dd",
+   "id": "58ac4bcd",
    "metadata": {},
    "outputs": [],
    "source": [
@@ -62,8 +62,6 @@
     "# Setup Flaks routes: one for the main HTML page for viewers, one to scrape new data using the written code.\n",
     "# Tells Flask what to display when looking @ the home page.\n",
     "@app.route(\"/\")\n",
-    "\n",
-    "\n",
     "def index():\n",
     "# uses pymongo to find the mars collection in our db.\n",
     "    mars = mongo.db.mars.find_one()\n",
@@ -74,30 +72,41 @@
   {
    "cell_type": "code",
    "execution_count": null,
-   "id": "87fd2857",
+   "id": "4b0113cf",
    "metadata": {},
    "outputs": [],
    "source": [
     "# Setup Scraping Route\n",
-    "# the button of the web app.\n",
+    "# The button of the web app.\n",
     "# Defines the route Flask will use.\n",
     "@app.route(\"/scrape\")\n",
     "def scrape()\n",
-    "# assin a new variable that points to our Mongo db\n",
+    "# assign a new variable that points to Mongo db.\n",
     "    mars = mongo.db.mars\n",
-    "# create a new variabl to hold the newly scraped data. reference the scrape_all func. in the scraping.py file exported from Jupyter.\n",
+    "# create new variable to hold the newly scraped data. reference the scrape_all func. in the scraping.py file exported from Jupyter.\n",
     "    mars_data = scraping.scrape_all()\n",
-    "# then update the db using .update_one.\n",
+    "# update the db using .update_one.\n",
     "    mars.update_one({}, {\"$set\":mars_data}, upsert=True)\n",
     "#     adds redirect after scraping data. navigate our page back to / where we can see updated content.\n",
-    "    return redirect('/', code=302)\n",
-    "\n"
+    "    return redirect('/', code=302)\n"
    ]
   },
   {
    "cell_type": "code",
    "execution_count": null,
-   "id": "92e817a8",
+   "id": "d83c73c3",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# tell Flask to run\n",
+    "if __name__ == \"__main__\"\n",
+    "    app.run"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b121be73",
    "metadata": {},
    "outputs": [],
    "source": []
